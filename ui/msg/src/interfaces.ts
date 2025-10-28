e696225ce9dd86598b2cf233c4f9ecab475b54e3
AuburnMedia <nicholashoughton2@gmail.com>
2025-10-29 09:04:05 +1100

    fix: backwards compat for existing records
    refs: lichess-org/lila/#18470

--- ui/msg/src/interfaces.ts
+++ ui/msg/src/interfaces.ts
@@ -17,11 +17,11 @@ export interface Me extends User {
   bot: boolean;
 }
 export interface Msg {
-  id: string;
+  id?: string;
   user: string;
   text: string;
   date: Date;
-  deleted: boolean;
+  deleted?: boolean;
 }
 export interface LastMsg extends Msg {
   read: boolean;
